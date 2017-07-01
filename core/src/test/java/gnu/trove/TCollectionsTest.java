e4da78df12515ee6c09b4419e5b7a6831b9895d4
Sebastian Kürten <sebastian@topobyte.de>
2017-07-01 16:37:39 +0200

    Eliminate the TCollections class
    Spread the utility methods contained in TCollections across various
    small utility classes.

--- core/src/test/java/gnu/trove/TCollectionsTest.java
+++ core/src/test/java/gnu/trove/TCollectionsTest.java
@@ -1,10 +1,13 @@
 package gnu.trove;
 
 import gnu.trove.list.TIntList;
+import gnu.trove.list.TUnmodifiableIntLists;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.TUnmodifiableIntObjectMaps;
 import gnu.trove.map.hash.TIntObjectHashMap;
 import gnu.trove.set.TIntSet;
+import gnu.trove.set.TUnmodifiableIntSets;
 import gnu.trove.set.hash.TIntHashSet;
 import junit.framework.TestCase;
 
@@ -16,8 +19,8 @@ public class TCollectionsTest extends TestCase {
 	public void testUnmodifiableList() {
 		final TIntArrayList one = new TIntArrayList( new int[]{ 1, 2, 3, 4 } );
 		final TIntArrayList two = new TIntArrayList( new int[]{ 1, 2, 3, 4 } );
-		TIntList uOne = TCollections.unmodifiableList( one );
-		TIntList uTwo = TCollections.unmodifiableList( two );
+		TIntList uOne = TUnmodifiableIntLists.wrap( one );
+		TIntList uTwo = TUnmodifiableIntLists.wrap( two );
 
 		assertEquals( one, two );
 		assertEquals( uOne, uTwo );
@@ -28,8 +31,8 @@ public class TCollectionsTest extends TestCase {
 	public void testUnmodifiableSet() {
 		final TIntSet one = new TIntHashSet( new int[]{ 1, 2, 3, 4 } );
 		final TIntSet two = new TIntHashSet( new int[]{ 1, 2, 3, 4 } );
-		TIntSet uOne = TCollections.unmodifiableSet( one );
-		TIntSet uTwo = TCollections.unmodifiableSet( two );
+		TIntSet uOne = TUnmodifiableIntSets.wrap( one );
+		TIntSet uTwo = TUnmodifiableIntSets.wrap( two );
 
 		assertEquals( one, two );
 		assertEquals( uOne, uTwo );
@@ -43,8 +46,8 @@ public class TCollectionsTest extends TestCase {
 		one.put( 2, Integer.valueOf( 2 ) );
 		one.put( 3, Integer.valueOf( 3 ) );
 		final TIntObjectMap<Integer> two = new TIntObjectHashMap<Integer>( one );
-		TIntObjectMap<Integer> uOne = TCollections.unmodifiableMap( one );
-		TIntObjectMap<Integer> uTwo = TCollections.unmodifiableMap( two );
+		TIntObjectMap<Integer> uOne = TUnmodifiableIntObjectMaps.wrap( one );
+		TIntObjectMap<Integer> uTwo = TUnmodifiableIntObjectMaps.wrap( two );
 
 		assertEquals( one, two );
 		assertEquals( uOne, uTwo );
